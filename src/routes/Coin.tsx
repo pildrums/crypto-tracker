108303a67f339bc71e0f79648f2c8b3da4291867
김필진 <pildrum@gimpiljin-ui-MacBookPro.local>
2022-09-28 16:54:57 +0900

    Coin.tsx UI modified, Coins.tsx -> Coin.tsx state dispatch, RouterState defined

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,51 @@
-import { useParams } from "react-router-dom";
+import { useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+interface RouterState {
+  state: string;
+}
 
 /**
  * @description 세부 컴포넌트
+ * @description 현재 secret 창에서 직접 url을 열어보면 에러 발생!
+ * @todo secret 창에서 url을 직접 입력했을 때 발생하는 에러 처리.
  */
 const Coin = () => {
   const { coinId } = useParams();
-  console.log(coinId);
-  return <div>Coin</div>;
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation() as RouterState;
+  return (
+    <Container>
+      <Header>
+        <Title>{state}</Title>
+      </Header>
+      {loading && <Loader>Loading...</Loader>}
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  padding: 0 10px;
+  margin: 0 auto;
+  max-width: 480px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -5,58 +6,61 @@ import styled from 'styled-components';
  * @constant coins - Dummy Data
  * @todo API Data 가져올 예정.
  */
-const coins = [
-  {
-    id: 'btc-bitcoin',
-    name: 'Bitcoin',
-    symbol: 'BTC',
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: 'coin',
-  },
-  {
-    id: 'eth-ethereum',
-    name: 'Ethereum',
-    symbol: 'ETH',
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: 'coin',
-  },
-  {
-    id: 'hex-hex',
-    name: 'HEX',
-    symbol: 'HEX',
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: 'token',
-  },
-];
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 /**
  * @description 메인 컴포넌트
  */
 const Coins = () => {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://api.coinpaprika.com/v1/coins');
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      <CoinList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinList>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`} state={coin.name}>
+                <Img
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                  alt="coinIcon"
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinList>
+      )}
     </Container>
   );
 };
 
 const Container = styled.div`
   padding: 0 10px;
+  margin: 0 auto;
+  max-width: 480px;
 `;
 
 const Header = styled.header`
@@ -70,17 +74,18 @@ const CoinList = styled.ul``;
 
 const Coin = styled.li`
   background: #fff;
-  color: ${props => props.theme.bgColor};
+  color: ${(props) => props.theme.bgColor};
   margin-bottom: 10px;
   border-radius: 15px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.5s ease-in;
-    display: block;
   }
   &:hover {
     a {
-      color: ${props => props.theme.accentColor};
+      color: ${(props) => props.theme.accentColor};
     }
   }
 `;
@@ -88,6 +93,17 @@ const Coin = styled.li`
 const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 export default Coins;
