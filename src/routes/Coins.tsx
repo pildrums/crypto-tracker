87c4a4684c81470b5261cdab7b546cda86cd4fd6
김필진 <pildrum@gimpiljin-ui-MacBookPro.local>
2022-09-28 15:06:12 +0900

    Coins.tsx(main component) styled done

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,13 +1,92 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+/**
+ * @constant coins - Dummy Data
+ * @todo API Data 가져올 예정.
+ */
+const coins = [
+  {
+    id: 'btc-bitcoin',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: 'coin',
+  },
+  {
+    id: 'eth-ethereum',
+    name: 'Ethereum',
+    symbol: 'ETH',
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: 'coin',
+  },
+  {
+    id: 'hex-hex',
+    name: 'HEX',
+    symbol: 'HEX',
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: 'token',
+  },
+];
 
 /**
  * @description 메인 컴포넌트
  */
 const Coins = () => {
-  return <Title>코인</Title>;
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinList>
+    </Container>
+  );
 };
 
+const Container = styled.div`
+  padding: 0 10px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  background: #fff;
+  color: ${props => props.theme.bgColor};
+  margin-bottom: 10px;
+  border-radius: 15px;
+  a {
+    padding: 20px;
+    transition: color 0.5s ease-in;
+    display: block;
+  }
+  &:hover {
+    a {
+      color: ${props => props.theme.accentColor};
+    }
+  }
+`;
+
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 
